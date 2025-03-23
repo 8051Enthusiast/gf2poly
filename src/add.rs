c8301e0eb2f57b064452b8710f7f32342f7462bc
8051enthusiast <8051enthusiast@protonmail.com>
2025-03-23 18:01:51 +0100

    add more add

--- src/add.rs
+++ src/add.rs
@@ -1,3 +1,5 @@
+// lol characteristic 2
+#![allow(clippy::suspicious_arithmetic_impl)]
 use crate::{LimbStorage, deg, normalized_len};
 
 use super::Gf2Poly;
@@ -8,10 +10,18 @@ impl core::ops::Add<&Gf2Poly> for &Gf2Poly {
     fn add(self, rhs: &Gf2Poly) -> Self::Output {
         let new_size = self.limbs().len().max(rhs.limbs().len());
         let mut limbs = LimbStorage::with_capacity(new_size);
-        limbs.extend(self.limbs().iter().zip(rhs.limbs().iter()).map(|(a, b)| a ^ b));
+        limbs.extend(
+            self.limbs()
+                .iter()
+                .zip(rhs.limbs().iter())
+                .map(|(a, b)| a ^ b),
+        );
         if rhs.limbs().len() == self.limbs().len() {
+            // only in this case can the highest terms cancel out
             limbs.truncate(normalized_len(&limbs));
         } else if limbs.len() < self.limbs().len() {
+            // the zip only wrote the common limbs, so here we add the rest
+            // from whichever polynomial was bigger
             limbs.extend_from_slice(&self.limbs()[limbs.len()..]);
         } else {
             limbs.extend_from_slice(&rhs.limbs()[limbs.len()..]);
@@ -30,8 +40,22 @@ impl core::ops::AddAssign<&Gf2Poly> for Gf2Poly {
         }
         if self.deg == rhs.deg {
             self.limbs.truncate(normalized_len(&self.limbs))
+        } else if rhs.deg() > self.deg() {
+            // in the case self.deg() > rhs.deg(), self already has the right
+            // upper bits, so we only have to handle this case
+            self.limbs
+                .extend_from_slice(&rhs.limbs()[self.limbs.len()..]);
         }
         self.deg = deg(&self.limbs);
+    }
+}
+
+impl core::ops::AddAssign<Gf2Poly> for Gf2Poly {
+    fn add_assign(&mut self, mut rhs: Gf2Poly) {
+        if rhs.deg() > self.deg() {
+            core::mem::swap(self, &mut rhs);
+        }
+        *self += &rhs;
     }
 }
 
@@ -79,6 +103,37 @@ impl core::ops::Neg for &Gf2Poly {
     }
 }
 
+impl core::ops::Sub<Gf2Poly> for Gf2Poly {
+    type Output = Gf2Poly;
+
+    fn sub(self, rhs: Gf2Poly) -> Self::Output {
+        self + rhs
+    }
+}
+
+impl core::ops::Sub<Gf2Poly> for &Gf2Poly {
+    type Output = Gf2Poly;
+
+    fn sub(self, rhs: Gf2Poly) -> Self::Output {
+        self + rhs
+    }
+}
+
+impl core::ops::Sub<&Gf2Poly> for Gf2Poly {
+    type Output = Gf2Poly;
+
+    fn sub(self, rhs: &Gf2Poly) -> Self::Output {
+        self + rhs
+    }
+}
+
+impl core::ops::Sub<&Gf2Poly> for &Gf2Poly {
+    type Output = Gf2Poly;
+
+    fn sub(self, rhs: &Gf2Poly) -> Self::Output {
+        self + rhs
+    }
+}
 #[cfg(test)]
 mod tests {
     use crate::prop_assert_poly_eq;
@@ -118,6 +173,20 @@ mod tests {
         #[test]
         fn char2(a: Gf2Poly) {
             prop_assert_poly_eq!(&a + &a, Gf2Poly::zero());
+        }
+
+        #[test]
+        fn add_assign(mut a: Gf2Poly, b: Gf2Poly) {
+            let c = &a + &b;
+            a += &b;
+            prop_assert_poly_eq!(a, c);
+        }
+
+        #[test]
+        fn add_assign_consume(mut a: Gf2Poly, b: Gf2Poly) {
+            let c = &a + &b;
+            a += b;
+            prop_assert_poly_eq!(a, c);
         }
     }
 }
